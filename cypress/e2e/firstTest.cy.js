8c71ac5c53422871c15c7da7d5f9ab6a625737f7
Bergomezz <bernardogomezz@yahoo.com.br>
2022-06-22 16:51:41 -0300

    Cypress with API

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+context('Starting New API Module', () => {
+  describe('Test with backend', () => {
+    beforeEach('login into the app', () => {
+      cy.intercept('GET', '**/tags', { fixture: 'tags.json' }).as('tagsMochs')
+      cy.loginToApplication()
+    })
+    it.skip('verify correct request and response', () => {
+      cy.intercept('POST', '**/articles').as('postArticles')
+
+      cy.contains('New Article').click()
+      cy.get('[formcontrolname="title"]').type('This is an article')
+      cy.get('[formcontrolname="description"]').type('This is a description')
+      cy.get('[formcontrolname="body"]').type('This is the body of the article')
+      cy.contains('Publish Article').click()
+
+      cy.wait('@postArticles')
+      cy.get('@postArticles').then(xhr => {
+        cy.writeFile('cypress/json/networkRes.json', xhr)
+        console.log(xhr)
+
+        // expect(xhr.status).to.equal(200)
+        expect(xhr.request.body.article.body).to.equal(
+          'This is the body of the article'
+        )
+        // expect(xhr.response.body.article.description).to.equal('This is the body of the article')
+      })
+    })
+
+    it.skip('should gave tags with routing objects', () => {
+      cy.wait('@tagsMochs')
+      cy.get('.tag-list')
+        .should('contain', 'cypress')
+        .and('contain', 'automation')
+        .and('contain', 'testing')
+        .and('contain', 'react')
+    })
+
+    it('verify global feed likes count', () => {
+      cy.intercept('GET', '**/articles/feed*', {
+        article: [],
+        articlesCount: 0
+      })
+      cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
+
+      cy.contains('Global Feed').click()
+      cy.get('app-article-list button').then(listOfButtons => {
+        expect(listOfButtons[0]).to.contain('2819')
+      })
+
+      cy.fixture('articles2').then(file => {
+        const articleLink = file.articles[0].slug
+        cy.intercept('POST', `**/articles/${articleLink}/favorite`, file)
+      })
+
+      cy.get('app-article-list button').eq(0).click().should('contain', '2820')
+    })
+  })
+})
